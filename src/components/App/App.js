a7ae091988b6e89f451fb28906dc862040e631fd
Gabriel Fernández Moral <gabriel.fernandez.moral@gmail.com>
2023-06-19 12:41:18 +0200

    initial clean up

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Game Affinity
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export { App };
