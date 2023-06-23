f153719d76b59c69863bb332e5765e4de138101d
Gabriel Fernández Moral <gabriel.fernandez.moral@gmail.com>
2023-06-23 14:32:16 +0200

    Improving game details modal

--- src/components/GameDetailsModal/index.js
+++ src/components/GameDetailsModal/index.js
@@ -12,10 +12,16 @@ function GameDetailsModal({ gameDetails, isModalOpen, setIsModalOpen }) {
                 open={isModalOpen}
                 onCancel={() => { setIsModalOpen(false) }}
                 footer={null}
-                width={700}>
-                <p>{gameDetails.description}</p>
-                <p>{gameDetails.metacritic}</p>
-                <p>{gameDetails.released}</p>
+                width={700}
+                centered={true}>
+                    <a href={gameDetails.metacritic_url}><p>Metacritic: {gameDetails.metacritic}</p></a>
+                    <img className="GameDetailsModalImage" alt="" src={gameDetails.background_image} />
+                    <p align="justify">{gameDetails.description_raw}</p>
+                    <p><span>Platforms: </span>{gameDetails.platforms.map(platformsItem => platformsItem.platform.name).join(', ')}</p>
+                    <p><span>Studio: </span>{gameDetails.developers[0].name}</p>
+                    <p><span>Released: </span>{gameDetails.released}</p>
+                    <p><span>Genres: </span>{gameDetails.genres.map(genresItem => genresItem.name).join(', ')}</p>
+                    <p><span>Tags: </span>{gameDetails.tags.map(tagsItem => tagsItem.name).join(', ')}</p>
             </Modal>
         </>)
         : ('')
